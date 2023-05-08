44bbbd2cc1afe191cdcc622933edee48139fbf81
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2023-05-08 18:08:13 -0300

    feat: create use case for get user by id

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,5 +1,5 @@
 import { prisma } from '@/lib/prisma'
-import { Prisma } from '@prisma/client'
+import { Prisma, User } from '@prisma/client'
 import { UsersRepository } from '../users-repository'
 
 export class PrismaUsersRepository implements UsersRepository {
@@ -11,6 +11,10 @@ export class PrismaUsersRepository implements UsersRepository {
     })
 
     return user
+  }
+
+  findById(id: string): Promise<User | null> {
+    throw new Error('Method not implemented.')
   }
 
   async create(data: Prisma.UserCreateInput) {
