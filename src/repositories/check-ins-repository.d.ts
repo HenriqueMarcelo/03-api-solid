0b11a2be1d448a3d0fff570cfcd0acf63193c896
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2023-05-09 10:30:27 -0300

    feat: feat check-ins history

--- src/repositories/check-ins-repository.d.ts
+++ src/repositories/check-ins-repository.d.ts
@@ -1,8 +1,7 @@
 import { CheckIn, Prisma } from '@prisma/client'
 
 export interface CheckInsRepository {
-  //   findById(id: string): Promise<User | null>
-  //   findByEmail(email: string): Promise<User | null>
   create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>
   findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>
+  findManyByUserId(userId: string): Promise<CheckIn[]>
 }
