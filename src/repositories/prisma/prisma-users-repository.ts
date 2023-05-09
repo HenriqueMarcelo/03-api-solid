3a59d432dde41ea142d0748bac079260a2b779d6
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2023-05-09 16:53:35 -0300

    feat: create prisma repository for check-in & user

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,5 +1,5 @@
 import { prisma } from '@/lib/prisma'
-import { Prisma, User } from '@prisma/client'
+import { Prisma } from '@prisma/client'
 import { UsersRepository } from '../users-repository'
 
 export class PrismaUsersRepository implements UsersRepository {
@@ -13,8 +13,14 @@ export class PrismaUsersRepository implements UsersRepository {
     return user
   }
 
-  findById(id: string): Promise<User | null> {
-    throw new Error('Method not implemented.')
+  async findById(id: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    return user
   }
 
   async create(data: Prisma.UserCreateInput) {
